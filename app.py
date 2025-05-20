fc3b8558f5a8bf0e80ebf0a25551b865c2966fce
Tone Alongkorn <alongkorn.chaichana@yahoo.com>
2025-05-20 22:00:18 +0700

    fix error jsonify

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 from flask import Flask,render_template,jsonify
-from database import load_jobs_form_db
+from database import load_jobs_from_db,load_job_from_db
 from sqlalchemy import text
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    jobs = load_jobs_form_db()
+    jobs = load_jobs_from_db()
     return render_template('home.html',jobs=jobs)
 
 @app.route("/api/jobs")
 def list_jobs():
-    jobs = load_jobs_form_db()
+    jobs = load_jobs_from_db()
     return jsonify(jobs)
 
-    
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    return jsonify(job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,15 +1,27 @@
 from sqlalchemy import create_engine, text
-
+import json
 engine = create_engine(
     "mysql+pymysql://root:@localhost/tone_careers?charset=utf8mb4"
 )
 
-def load_jobs_form_db():
+def load_jobs_from_db():
     with engine.connect() as conn:
-        jobs = conn.execute(text("select * from jobs"))
-        # print(jobs.all())
+        result = conn.execute(text("SELECT * FROM jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._mapping))
         return jobs
 
-# with engine.connect() as conn:
-#     result = conn.execute(text("select * from jobs"))
-#     print(result.all())
+
+
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT * FROM  jobs WHERE id = :val"),
+           {"val":id})
+        rows = result.all()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0]._mapping)
+
+
